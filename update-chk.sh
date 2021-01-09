762416c280e15dd1061986e06c7602b441a19d1e
X1pe0 <9041120+X1pe0@users.noreply.github.com>
2021-01-08 19:36:01 -0600

    Update update-chk.sh

--- update-chk.sh
+++ update-chk.sh
@@ -9,6 +9,7 @@ sed -i '/Listing.../d' $LOG_FILE
 echo $LOG_FILE | tee /dev/fd/3
 
 #Static needed for Automate. Leaving original for history. 
+> $STATIC_LOG_FILE
 exec 3>&1 1>>${STATIC_LOG_FILE} 2>&1
 apt list --upgradable | column -s "/" -t| column -s "[" -t | tee /dev/fd/3
 sed -i '/WARNING: apt does not have a stable CLI interface. Use with caution in scripts./d' $STATIC_LOG_FILE
